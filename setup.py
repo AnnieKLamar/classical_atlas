c3f28c458cd991bab882c589497352296f44c0a3
AnnieKLamar <kalamar@stanford.edu>
2022-08-26 12:51:08 -0700

    packaging fixes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'classical_atlas',         # How you named your package folder (MyLib)
   packages = ['classical_atlas'],   # Chose the same as "name"
-  version = '1.0',      # Start with a small number and increase it with every change you make
+  version = '1.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A Python package for accessing open-source geospatial datasets.',   # Give a short description about your library
   author = 'Annie K. Lamar',                   # Type in your name
